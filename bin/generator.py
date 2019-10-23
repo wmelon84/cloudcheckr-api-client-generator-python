b0fd8193b4c39985bb12bd46bc92d1ee3447a57f
avelino© <avelino.collar@ingrammicro.com>
2019-10-23 17:45:17 +0200

    Simplify generator script. Normalize and remove duplicated parameters.

--- bin/generator.py
+++ bin/generator.py
@@ -9,20 +9,29 @@ import requests
 import sys
 
 BASE_PATH = '../build/cloudcheckr_api_client/controllers/'
+
 IMPORTS = '''import requests
 from ._utils import get_non_blank_parameters, from_str_to_obj\n\n'''
+
 UTILS_FILE = '_utils.py'
+
 UTILS_SOURCE_CODE = '''import collections
 import inflection
 import json\n\n
 def from_str_to_obj(response=''): 
     return json.loads(response, object_hook=lambda r: collections.namedtuple(
-        'r', 
+        'response', 
         [inflection.underscore(key) for key in r.keys()])(*r.values()))\n\n 
-def get_non_blank_parameters(all_parameters=None):
+def get_non_blank_parameters(all_parameters, normalized_parameters=None):
     if all_parameters:
-        return {k.rstrip('_'): v for k, v in all_parameters.items() if v and v.strip() and k != 'access_key'}
-    return {}\n\n'''
+        return {denormalize(k, normalized_parameters): v for k, v in all_parameters.items()
+                if v and v.strip() and k != 'access_key' and k != 'normalized_parameters' and k != 'env'}
+    return {}\n\n
+def denormalize(key, normalized_parameters=None):
+    return normalized_parameters[key] if normalized_parameters and key in normalized_parameters.keys() \
+        else key.rstrip('_')\n\n'''
+
+PARAMETER_NAME_EXCEPTIONS = {'thirdpartyaccountids': 'third_party_account_ids'}
 
 all_api_endpoints = ''
 if len(sys.argv) > 1:
@@ -42,17 +51,16 @@ controllers = json.loads(all_api_endpoints,
 
 
 def clean_parameter_name(parameter_name):
-    clean_name = parameter_name.replace('(required)', '').replace('(admin level)', '')
-    return clean_name + '_' if keyword.iskeyword(clean_name) or clean_name in dir(builtins) else clean_name
+    cleaned_name = parameter_name.replace('(required)', '').replace('(admin level)', '')
+    return cleaned_name + '_' if keyword.iskeyword(cleaned_name) or cleaned_name in dir(builtins) else cleaned_name
 
 
-def is_camel_case(parameter_name):
-    return inflection.camelize(parameter_name, uppercase_first_letter=False) == parameter_name \
-           or inflection.camelize(parameter_name) == parameter_name
+def is_not_normalized(parameter_name):
+    return inflection.camelize(parameter_name) == parameter_name
 
 
-def to_snake_case(parameter_name):
-    return inflection.underscore(parameter_name)
+def normalize(string):
+    return inflection.underscore(string)
 
 
 #  create base directory
@@ -72,21 +80,48 @@ for c in controllers:
     seen_api_calls = []
     for call in c.api_calls:
         if call.method_name not in seen_api_calls and not call.method_warning:
+
+            # saved already processed methods to avoid duplicates
             seen_api_calls.append(call.method_name)
-            source_code += 'def ' + inflection.underscore(call.method_name) + '(\n'
-            required_list = list(
-                map(lambda p: clean_parameter_name(p),
-                    filter(lambda p: '(required)' in p, call.param_names)))
-            optional_list = list(
-                map(lambda p: clean_parameter_name(p) + '=None',
-                    filter(lambda p: '(required)' not in p, call.param_names)))
-            source_code += ',\n'.join(required_list) if required_list else ''
-            source_code += ',\n' + ',\n'.join(optional_list) if optional_list else ''
-            source_code += ",\nenv='https://api.cloudcheckr.com/api/'):\n"
-            # HTTP method works also for GET methods
-            source_code += '    response = requests.post(\n'
+
+            required_parameters = []
+            optional_parameters = []
+            normalized_parameters = {}
+
+            for name in call.param_names:
+
+                if name in PARAMETER_NAME_EXCEPTIONS.keys():
+                    clean_name = PARAMETER_NAME_EXCEPTIONS[name]
+                    normalized_clean_name = PARAMETER_NAME_EXCEPTIONS[name]
+                    normalized_parameters[normalized_clean_name] = name
+                else:
+                    clean_name = clean_parameter_name(name)
+                    normalized_clean_name = clean_parameter_name(normalize(name))
+                if '(required)' in name:
+                    required_parameters.append(normalized_clean_name)
+                else:
+                    if normalized_clean_name not in required_parameters \
+                            and normalized_clean_name not in optional_parameters:
+                        optional_parameters.append(normalized_clean_name)
+
+                if is_not_normalized(clean_name):
+                    normalized_parameters[normalized_clean_name] = clean_name
+
+            source_code += 'def ' + normalize(call.method_name) + '(\n'
+
+            if required_parameters:
+                source_code += ',\n'.join(required_parameters) + ',\n'
+            if optional_parameters:
+                source_code += ',\n'.join([p + '=None' for p in optional_parameters]) + ',\n'
+            source_code += "env='https://api.cloudcheckr.com/api/'):\n"
+            if normalized_parameters:
+                source_code += '    normalized_parameters = ' + json.dumps(normalized_parameters) + '\n'
+
+            source_code += '    response = requests.' + (
+                'get' if + call.method_name.startswith('get') else 'post') + '(\n'
             source_code += 'env + CONTROLLER_NAME + \'' + call.method_name + '\',\n'
-            source_code += 'params=get_non_blank_parameters(locals()),\n'
+            source_code += 'params=get_non_blank_parameters(locals()' + \
+                           (', normalized_parameters' if normalized_parameters else '') + '),\n'
             source_code += 'headers={\'access_key\': access_key})\n'
             source_code += '    response.raise_for_status()\n'
             source_code += '    return from_str_to_obj(response.text)\n\n\n'
